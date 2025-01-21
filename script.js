360d03381a75acba6f7e6ea710f8467c13ec4882
DavidMiller <dmiller@sharetechindy.com>
2025-01-20 20:21:28 -0500

    changed media query to include flip phone size

--- script.js
+++ script.js
@@ -39,29 +39,7 @@ document.addEventListener('scroll', () => {
 });
 
 
-            const API_KEY = 'c5bc119317899a12ba1c6ba2e2c00fc8';
-            const BIBLE_ID = 'de4e12af7f28f599-02'; // Example Bible ID
-            const VERSES = [
-                'JER.29.11', 'PSA.23', '1COR.4.4-8', 'PHP.4.13', 'JHN.3.16',
-                'ROM.8.28', 'ISA.41.10', 'PSA.46.1', 'GAL.5.22-23', 'HEB.11.1',
-                '2TI.1.7', '1COR.10.13', 'PRO.22.6', 'ISA.40.31', 'JOS.1.9',
-                'HEB.12.2', 'MAT.11.28', 'ROM.10.9-10', 'PHP.2.3-4', 'MAT.5.43-44'
-            ];
-    
-            const verseIndex = Math.floor(Math.random() * VERSES.length);
-            const verseID = VERSES[verseIndex];
-    
-            fetch(`https://api.scripture.api.bible/v1/bibles/${BIBLE_ID}/verses/${verseID}`, {
-                headers: {
-                    'api-key': API_KEY
-                }
-            })
-            .then(response => response.json())
-            .then(data => {
-                document.getElementById('verse').innerText = data.data.content;
-            })
-            .catch(error => console.error('Error fetching verse:', error));
-        
+          
             (function() {
                 $(document ).ready(function() {
                     getVerse();
